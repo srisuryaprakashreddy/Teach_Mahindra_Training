3d0c181fe57969e74e0fae9308717dcf6d908e8b
sri surya prakash reddy <108176778+srisuryaprakashreddy@users.noreply.github.com>
2025-06-24 15:33:27 +0530

    Update AdditionExample.java

--- JAVA/Day1/AdditionExample.java
+++ JAVA/Day1/AdditionExample.java
@@ -6,10 +6,10 @@ public class AdditionExample {
         Scanner scanner = new Scanner(System.in);
 
         // Ask the user for two numbers
-        System.out.print("Enter first number: ");
+        // System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
 
-        System.out.print("Enter second number: ");
+        // System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
         // Perform addition
